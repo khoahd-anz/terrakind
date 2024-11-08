622091c06c8197e66b236ae37f0d4daff00c7f1f
khoahd-anz <huynhdang.khoa@anz.com>
2024-11-08 16:16:29 +0700

    feat: add input variable for kind provider

---
+++ clusters/providers.tf
@@ -0,0 +1,4 @@
+# config provider here
+# NOTE: provider "kind" is used to create a local k8s cluster can be a binary use some API call or spin up thing local
+
+provider "kind" {}
---
+++ clusters/variables.tf
@@ -0,0 +1,30 @@
+variable "clusters" {
+  type = list(object({
+    name       = string
+    node_image = optional(string, "kindest/node:v1.31.0")
+    nodes = list(object({
+      role                   = string
+      kubeadm_config_patches = optional(list(string))
+      extra_port_mappings = optional(list(object({
+        listen_address = optional(string, "0.0.0.0")
+        container_port = string
+        host_port      = string
+        protocol       = string
+      })), [])
+    }))
+  }))
+  default = []
+
+  validation {
+    condition = alltrue(
+        [
+            for cluster in var.clusters : alltrue(
+                [for node in cluster.nodes : contains(
+                    ["control-plane", "worker"], node.role)
+                ]
+            )
+        ]
+    )
+    error_message = "Invalid node type, nodes can only be \"control-plane\" or \"worker\"."
+  }
+}
